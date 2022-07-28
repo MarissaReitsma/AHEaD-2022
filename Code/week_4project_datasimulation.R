92ce37901abae93d5d155221c92d353003679712
mileatim <mileatimelese.2050@gmail.com>
2022-07-27 18:44:33 -0600

    Updated_simulated_data

--- Code/week_4project_datasimulation.R
+++ Code/week_4project_datasimulation.R
@@ -1,5 +1,5 @@
 library(tidyverse)
-
+library("bindata")
 
 set.seed(600)
 
@@ -46,27 +46,61 @@ set.seed(610)
 n=1000000
 age_90_and_above <-rbinom(n,1,0.049)
 
-summary(age_65_69)
-summary(age_70_79)
-summary(age_80_89)
-summary(age_90_and_above)
-year<- rep(c(2016), times=1000000)
-simulated_data_table <-data.frame (year, sex_2016, arthritis_2016, cancer_2016, diabetes_2016, age_65_69, age_70_79, age_80_89, age_90_and_above )
-colnames(simulated_data_table) <- c("Year", "Documented_Female_Sex", "Arthritis", "Cancer", "Diabetes", "age:65-69", "age:70-79", "age:80-89", "age:>90")
+
+
+set.seed(610)
+n=1000000
+hypertention <- rbinom (n, 1, 457915/1394701 )
+
+
+set.seed(610)
+n=1000000
+coronary <- rbinom (n, 1, 169807/1394701)
+
+
+set.seed(610)
+n=1000000
+musculoskeletal_issues <- rbinom(n, 1, 376220/1394701)
+
+
+set.seed(610)
+n=1000000
+symptoms_of_abnormality <- rbinom(n, 1, 517269/1394701)
+
+
+set.seed(610)
+n=1000000
+Endocronic_metabolic_nutritional_disorders <- rbinom(n, 1, 340288/1394701)
+
+simulated_data_table <-data.frame ( sex_2016, arthritis_2016, cancer_2016, diabetes_2016, age_65_69, age_70_79, age_80_89, age_90_and_above, hypertention, coronary, musculoskeletal_issues, symptoms_of_abnormality, Endocronic_metabolic_nutritional_disorders)
+colnames(simulated_data_table) <- c("Documented_Female_Sex", "Arthritis", "Cancer", "Diabetes", "age:65-69", "age:70-79", "age:80-89", "age:>90", "hypertention", "coronary", "Musculoskeletal_issues", "symptoms_of_abnormality", "Endocronic_metabolic_nutritional_disorders")
 simulated_data_table
 
 ##mean_each_variable_distribution
-sex<-mean(sex_2016) 
-arthritis<-mean(arthritis_2016)
-cancer<-mean(cancer_2016) 
-diabetes<-mean(diabetes_2016)
-age_65<-mean(age_65_69)
-age_70 <- mean(age_70_79)
-age_80 <- mean(age_80_89)
-age_above_90 <-mean(age_90_and_above)
+margin_probability <- colMeans(simulated_data_table) ##generates marginal probability of each variable
+margin_probability
+cond_mat <- condprob(simulated_data_table)  ##generates conditional probabilities matrix
+cond_mat
+com_prob_mat <- cond_mat*marg_probs ## Scale by marginal probabilities to produce common_prob matrix input
+com_prob_mat
+cor_mat <- cor(simulated_data_table, method = "pearson")  ##generates a correlation matrix
+cor_mat
+bincorr2commonprob(marg_probs, cor_mat) ## Use the built-in function to convert the binary correlation matrix to common prob matrix
+com_prob_mat
 
-summarizing_data <- matrix(c(sex, arthritis, cancer, diabetes, age_65, age_70, age_80, age_above_90), ncol = 1, byrow=TRUE)
-colnames(summarizing_data) <- c("mean value of each data set distribution")
-rownames(summarizing_data) <- c("Documented_Female_Sex", "Arthritis", "Cancer", "Diabetes", "age:65-69", "age:70-79", "age:80-89", "age:>90")
-table_data <- as.table(summarizing_data)
-table_data
+set.seed(12345)
+n=1000000
+correlated_binarydata <- rmvbin(n, margprob=margin_probability, commonprob = com_prob_mat)
+correlated_binarydata
+set.seed(12345)
+test2 <- rmvbin(n, margprob = margin_probability, bincorr = cor_mat)
+test2
+
+all.equal(correlated_binarydata, test2)
+
+
+
+
+
+       
+
